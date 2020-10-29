59e6e50666330319d9fc9b93b5d999998e8ee66b
SuperAlina2020 <alinagithub@gmail.com>
2020-10-29 19:15:42 +0600

    import models

--- online_shop/store/models.py
+++ online_shop/store/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200,null=True)
+
 class Product(models.Model):
     CATEGORY = (
         ('Телефон','Телефон'),
@@ -9,12 +12,44 @@ class Product(models.Model):
 
     )
     name = models.CharField(max_length=50,null=True)
+    price = models.FloatField(null=True)
     category = models.CharField(max_length=50,null=True,choices=CATEGORY)
-    description = models.CharField(max_length=50,null=True)
+    description = models.CharField(max_length=50,null=True,blank=True)
     date_created = models.DateTimeField(auto_now_add=True,null=True)
+    tag = models.ManyToManyField(Tag,null=True)
 
     def __str__(self):
         return  self.name
+
+class Customer(models.Model):
+
+    full_name = models.CharField(max_length=200, null=True)
+    surname = models.CharField(max_length=200,null=True)
+    phone = models.CharField(max_length=200,null=True)
+    email = models.CharField(max_length=200, null=True)
+    addres = models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.full_name
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Delivered', 'Delivered'),
+        ('Not delivered', 'Not delivered'),
+
+    )
+    customer = models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=200,null=True,choices=STATUS)
+
+    def __str__(self):
+        return self.product.name
+
+
+
+
 
 
 
