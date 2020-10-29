e1dc2ceab0b642847b1deeffe7258b8e38b77b29
SuperAlina2020 <alinagithub@gmail.com>
2020-10-29 19:09:42 +0600

    filter forms

---
+++ online_shop/store/filters.py
@@ -0,0 +1,13 @@
+from django_filters import FilterSet
+from .models import *
+from django.forms import DateField
+
+
+class OrderFilterSet(FilterSet):
+   #  start_date = DateField()
+   #  end_date = DateField()
+    class Meta:
+        model = Order
+        fields = '__all__'
+        exclude = ['customer',]
+
