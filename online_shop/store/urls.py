53c070d49d4f04c6ddfdec5bdd230e20baae2636
SuperAlina2020 <alinagithub@gmail.com>
2020-11-05 19:14:34 +0600

    added ways to reset password, reset password done, reset passwort form and reset password send

--- online_shop/store/urls.py
+++ online_shop/store/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from .views import *
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('',home_page,name='home'),
@@ -9,6 +11,10 @@ urlpatterns = [
     path('login/',login_page,name='login'),
     path('logout/',logout_page,name='logout'),
     path('user/',user_page,name='user'),
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name='store/reset_password.html'),name='password_reset'),
+    path('reset_password_done/',auth_views.PasswordResetDoneView.as_view(template_name='store/reset_password_done.html'),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='store/reset_password_form.html'),name='password_reset_confirm'),
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name='store/reset_password_send.html'),name='password_reset_complete'),
     path('settings/',account_settings,name='settings'),
     path('customer/<int:pk>/',customer_page,name='customer'),
     path('create_order/<int:pk>/',create_order,name='create_order'),
